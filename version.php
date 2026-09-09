3c351c139dfd1a649cbcc0462d5bdf7a817b6ed0
Alexssander <167444465+AlexssanderTnS@users.noreply.github.com>
2026-09-09 12:34:47 -0300

    feat: initialize theme-independent IBAP navigation plugin

---
+++ version.php
@@ -0,0 +1,15 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_ibapnav';
+$plugin->version = 2026090900;
+$plugin->requires = 2024042200; // Moodle 4.4 or later.
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0.0';
