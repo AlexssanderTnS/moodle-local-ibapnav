3c59e49859809946b897dd1c7be8445c390ac094
Alexssander <167444465+AlexssanderTnS@users.noreply.github.com>
2026-09-09 12:37:47 -0300

    feat: add Brazilian Portuguese strings

---
+++ lang/pt_br/local_ibapnav.php
@@ -0,0 +1,15 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Navegação de conteúdo IBAP';
+$string['previous'] = 'Voltar';
+$string['next'] = 'Continuar';
+$string['coursehome'] = 'Página inicial da jornada';
+$string['navigationaria'] = 'Navegação entre conteúdos do curso';
+$string['showcourse'] = 'Mostrar botão da página inicial da jornada';
+$string['showcourse_desc'] = 'Exibe um botão central que leva o aluno de volta à página principal do curso.';
+$string['showactivityname'] = 'Mostrar nome da atividade';
+$string['showactivityname_desc'] = 'Exibe o nome da atividade anterior e da próxima atividade junto aos botões de navegação.';
+$string['privacy:metadata'] = 'O plugin Navegação de conteúdo IBAP não armazena dados pessoais.';
