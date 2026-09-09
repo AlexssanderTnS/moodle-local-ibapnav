d0cdea2a3a6f767a2c85d01d03aabf39c7496a32
Alexssander <167444465+AlexssanderTnS@users.noreply.github.com>
2026-09-09 14:55:43 -0300

    chore: release block_ibapnav v2.1.0

--- version.php
+++ version.php
@@ -4,7 +4,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_ibapnav';
-$plugin->version = 2026090903;
+$plugin->version = 2026090904;
 $plugin->requires = 2024042200; // Moodle 4.4 or later.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.0.0';
+$plugin->release = '2.1.0';
