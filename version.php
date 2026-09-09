e1738c5d757a5d21a6dfc3e0e92b6aca63448a7a
Alexssander <167444465+AlexssanderTnS@users.noreply.github.com>
2026-09-09 12:42:47 -0300

    chore: bump plugin version after hook changes

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_ibapnav';
-$plugin->version = 2026090900;
+$plugin->version = 2026090901;
 $plugin->requires = 2024042200; // Moodle 4.4 or later.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.0';
+$plugin->release = '1.0.1';
