360259c11e94be7a5a3d5632634beeb5922758f2
Alexssander <167444465+AlexssanderTnS@users.noreply.github.com>
2026-09-09 14:21:28 -0300

    refactor: convert IBAP navigation to block plugin

--- version.php
+++ version.php
@@ -1,15 +1,10 @@
 <?php
 // This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'local_ibapnav';
-$plugin->version = 2026090902;
+$plugin->component = 'block_ibapnav';
+$plugin->version = 2026090903;
 $plugin->requires = 2024042200; // Moodle 4.4 or later.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.2';
+$plugin->release = '2.0.0';
