9a3dc0c73c793ca407041e8c0c9b6f548a150e29
Alexssander <167444465+AlexssanderTnS@users.noreply.github.com>
2026-09-09 14:22:52 -0300

    feat: add block capabilities

---
+++ db/access.php
@@ -0,0 +1,25 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = [
+    'block/ibapnav:addinstance' => [
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => [
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        ],
+        'clonepermissionsfrom' => 'moodle/site:manageblocks',
+    ],
+    'block/ibapnav:myaddinstance' => [
+        'riskbitmask' => RISK_SPAM,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_SYSTEM,
+        'archetypes' => [
+            'user' => CAP_PREVENT,
+        ],
+    ],
+];
