567c53c67bcb2615efb8807ff3a159e1997ca6fd
Alexssander <167444465+AlexssanderTnS@users.noreply.github.com>
2026-09-09 14:22:29 -0300

    refactor: move settings to block component

--- settings.php
+++ settings.php
@@ -4,21 +4,19 @@
 defined('MOODLE_INTERNAL') || die();
 
 if ($hassiteconfig) {
-    $settings = new admin_settingpage('local_ibapnav', get_string('pluginname', 'local_ibapnav'));
+    $settings = new admin_settingpage('block_ibapnav', get_string('pluginname', 'block_ibapnav'));
 
     $settings->add(new admin_setting_configcheckbox(
-        'local_ibapnav/showcourse',
-        get_string('showcourse', 'local_ibapnav'),
-        get_string('showcourse_desc', 'local_ibapnav'),
+        'block_ibapnav/showcourse',
+        get_string('showcourse', 'block_ibapnav'),
+        get_string('showcourse_desc', 'block_ibapnav'),
         1
     ));
 
     $settings->add(new admin_setting_configcheckbox(
-        'local_ibapnav/showactivityname',
-        get_string('showactivityname', 'local_ibapnav'),
-        get_string('showactivityname_desc', 'local_ibapnav'),
+        'block_ibapnav/showactivityname',
+        get_string('showactivityname', 'block_ibapnav'),
+        get_string('showactivityname_desc', 'block_ibapnav'),
         0
     ));
-
-    $ADMIN->add('localplugins', $settings);
 }
