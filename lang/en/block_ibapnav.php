aff8b7e0d15230d01793473b36bfff152f036448
Alexssander <167444465+AlexssanderTnS@users.noreply.github.com>
2026-09-09 14:23:12 -0300

    feat: add English strings for block plugin

---
+++ lang/en/block_ibapnav.php
@@ -0,0 +1,18 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'IBAP content navigation';
+$string['ibapnav:addinstance'] = 'Add IBAP navigation block';
+$string['ibapnav:myaddinstance'] = 'Add IBAP navigation block to Dashboard';
+$string['previous'] = 'Back';
+$string['next'] = 'Continue';
+$string['coursehome'] = 'Journey home';
+$string['navigationaria'] = 'Course content navigation';
+$string['showcourse'] = 'Show journey home button';
+$string['showcourse_desc'] = 'Shows a central button linking back to the main course page.';
+$string['showactivityname'] = 'Show activity name';
+$string['showactivityname_desc'] = 'Shows the previous and next activity names next to navigation controls.';
+$string['blockenabledinfo'] = 'IBAP navigation is enabled for this course. Buttons appear at the end of activities.';
+$string['privacy:metadata'] = 'The IBAP content navigation plugin does not store personal data.';
