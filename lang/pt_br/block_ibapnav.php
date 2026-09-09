5dab7745c0f5175e3d8b32b9519fd2dfdafb9b2e
Alexssander <167444465+AlexssanderTnS@users.noreply.github.com>
2026-09-09 14:23:01 -0300

    feat: add Portuguese strings for block plugin

---
+++ lang/pt_br/block_ibapnav.php
@@ -0,0 +1,18 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Navegação de conteúdo IBAP';
+$string['ibapnav:addinstance'] = 'Adicionar bloco de navegação IBAP';
+$string['ibapnav:myaddinstance'] = 'Adicionar bloco de navegação IBAP ao Painel';
+$string['previous'] = 'Voltar';
+$string['next'] = 'Continuar';
+$string['coursehome'] = 'Página inicial da jornada';
+$string['navigationaria'] = 'Navegação entre conteúdos do curso';
+$string['showcourse'] = 'Mostrar botão da página inicial da jornada';
+$string['showcourse_desc'] = 'Exibe um botão central que leva o aluno de volta à página principal do curso.';
+$string['showactivityname'] = 'Mostrar nome da atividade';
+$string['showactivityname_desc'] = 'Exibe o nome da atividade anterior e da próxima atividade junto aos botões.';
+$string['blockenabledinfo'] = 'Navegação IBAP habilitada neste curso. Os botões aparecem no final das atividades.';
+$string['privacy:metadata'] = 'O plugin Navegação de conteúdo IBAP não armazena dados pessoais.';
